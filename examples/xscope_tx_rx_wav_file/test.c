97b39fe7bfdcfb045695454c8379efa4589edf3b
Keith Au <keithau@xmos.com>
2023-08-22 12:22:42 +0800

    all bit rate works

--- examples/xscope_tx_rx_wav_file/test.c
+++ examples/xscope_tx_rx_wav_file/test.c
@@ -10,27 +10,87 @@
 
 #include <assert.h>
 
-#define appconfINPUT_FILENAME    "testin.wav"
+#define appconfINPUT_FILENAME    "testin_ch6_32bit.wav"
 #define appconfOUTPUT_FILENAME   "testout.wav"
 
-// #define appconfMAX_CHANNELS 1
 #define appconfOUT_NUM_OF_CHANNELS 2
 #define appconfFRAME_ADVANCE 240
 #define appconfFRAME_ELEMENT_SIZE sizeof(int32_t)
-#define appconfDATA_FRAME_SIZE_BYTES   (appconfFRAME_ADVANCE * appconfFRAME_ELEMENT_SIZE)
+#define appconfDATA_FRAME_SIZE_BYTES   (appconfFRAME_ADVANCE * appconfOUT_NUM_OF_CHANNELS * appconfFRAME_ELEMENT_SIZE)
 
 //input buffer and output buffer are interleaved as wav file
-void process_frame(uint8_t * input_buf, uint8_t * output_buf)
+void process_frame(void* input_buf, void* output_buf, short num_ch_out, short sampleWidth)
 {
-    unsigned i;
-    for(i=0;i<appconfDATA_FRAME_SIZE_BYTES;i++){
-        output_buf[i] = input_buf[i];   //add audio processing here
+    unsigned i, j;
+    size_t offset;
+
+    for(i=0;i<num_ch_out;i++){
+        for(j=0;j<appconfFRAME_ADVANCE;j++){
+            //add audio processing here
+
+            switch(sampleWidth){
+                case 16:
+                    offset =  (i * appconfFRAME_ADVANCE + j) * (sampleWidth/8);
+                    *((uint16_t *)(output_buf + offset))=*((uint16_t *)(input_buf + offset));
+                    break;
+                case 24:
+                    offset =  (i * appconfFRAME_ADVANCE + j) * (32/8);
+                    *((uint32_t *)(output_buf + offset))=*((uint32_t *)(input_buf + offset));
+                    break;
+                case 32:
+                    offset =  (i * appconfFRAME_ADVANCE + j) * (sampleWidth/8);
+                    *((uint32_t *)(output_buf + offset))=*((uint32_t *)(input_buf + offset));
+                    break;
+            }
+        }
+    }
+
+}
+
+//interleaveFrame for sending back to xscope output
+void interleaveFrame(uint8_t* interleavedFrame, void* deinterleavedFrames, unsigned frameSize, short numChannels, short sampleWidth)
+{
+    int i, j;
+
+    // Calculate the number of bytes per sample
+    int bytesPerSample = sampleWidth / 8;
+     
+    uint16_t temp_interleaved_sample_16;
+    uint32_t temp_interleaved_sample_32;
+
+
+    for (i = 0; i < frameSize; i++) {
+        for (j = 0; j < numChannels; j++) {
+            uint8_t* samplePtr = interleavedFrame + (i * numChannels + j) * bytesPerSample;
+
+            // Convert the processed audio buffer back to interleaved samples for sending back to xscope
+            switch (sampleWidth) {
+                case 16:
+                    temp_interleaved_sample_16 = *((uint16_t*)(deinterleavedFrames + (j * appconfFRAME_ADVANCE + i)*bytesPerSample));
+                    *samplePtr = temp_interleaved_sample_16 & 0xFF;
+                    *(samplePtr+1) = (temp_interleaved_sample_16 >> 8) & 0xFF;
+                    break;
+                case 24:
+                    temp_interleaved_sample_32 = *((uint32_t*)(deinterleavedFrames + (j * appconfFRAME_ADVANCE + i)*(32/8)));
+                    *samplePtr = temp_interleaved_sample_32 & 0xFF;
+                    *(samplePtr+1) = (temp_interleaved_sample_32 >> 8) & 0xFF;
+                    *(samplePtr+2) = (temp_interleaved_sample_32 >> 16) & 0xFF;
+                    break;
+                case 32:
+                    temp_interleaved_sample_32 = *((uint32_t*)(deinterleavedFrames + (j * appconfFRAME_ADVANCE + i)*bytesPerSample));
+                    *samplePtr = temp_interleaved_sample_32 & 0xFF;
+                    *(samplePtr+1) = (temp_interleaved_sample_32 >> 8) & 0xFF;
+                    *(samplePtr+2) = (temp_interleaved_sample_32 >> 16) & 0xFF;
+                    *(samplePtr+3) = (temp_interleaved_sample_32 >> 24) & 0xFF;
+                    break;
+            }
+        }
     }
 }
 
 
 
-void deinterleaveFrame(const uint8_t* interleavedFrame, uint16_t* deinterleavedFrames, unsigned frameSize, short numChannels, short sampleWidth)
+void deinterleaveFrame(const uint8_t* interleavedFrame, void* deinterleavedFrames, unsigned frameSize, short numChannels, short sampleWidth)
 {
     int i, j;
 
@@ -43,26 +103,16 @@ void deinterleaveFrame(const uint8_t* interleavedFrame, uint16_t* deinterleavedF
         for (j = 0; j < numChannels; j++) {
             const uint8_t* samplePtr = interleavedFrame + (i * numChannels + j) * bytesPerSample;
 
-            // Convert the interleaved sample bytes to a uint8_t value
             switch (sampleWidth) {
                 case 16:
                     *((uint16_t*) deinterleavedFrames + (j * appconfFRAME_ADVANCE) + i )= *((uint16_t*)samplePtr);
                     break;
                 case 24:
-                    *((uint16_t*) deinterleavedFrames + (j * appconfFRAME_ADVANCE) + i )=
-                        (uint16_t)(samplePtr[0] | (samplePtr[1] << 8) | (samplePtr[2] << 16));
+                    *(((uint32_t*) deinterleavedFrames) + (j * appconfFRAME_ADVANCE) + i )=
+                        (uint32_t)(samplePtr[0] | (samplePtr[1] << 8) | (samplePtr[2] << 16));
                     break;
                 case 32:
-                    // *((uint32_t*) deinterleavedFrames + (j * appconfFRAME_ADVANCE) + i  )= *((uint32_t*)samplePtr);
-                    deinterleavedFrames[j * appconfFRAME_ADVANCE + i]  = *((uint32_t*)samplePtr);
-                    if(((i * numChannels + j) ) == 5 )
-                    {
-                        printf("data 5 is 0x%x\n",*((uint32_t*)samplePtr));
-                        // deinterleavedFrames[0]=1;
-                        // deinterleavedFrames[1]=2;
-                        // deinterleavedFrames[5]=3;
-                        printf("deinterleavedFrames[1] is %d\n",deinterleavedFrames[1]);
-                    }
+                    *(((uint32_t*)deinterleavedFrames) + (j * appconfFRAME_ADVANCE) + i )  = *((uint32_t*)samplePtr);
                     break;
                 default:
                     // Unsupported sample width
@@ -83,18 +133,15 @@ void audio_proc_test(void){
     unsigned frame_count;
     unsigned block_count;
     unsigned bytes_per_block;
-    unsigned samples_per_block;
     size_t bytes_read = 0;
-    unsigned y=0;
 
     uint8_t * p_in_buf;
-    uint16_t * p_audio_in_channels;     //audio buffer of one block with samples arranged as channels
+    uint16_t * p_audio_in_channels_16;     //audio buffer of one block with samples arranged as channels, for sample width of 16bit
+    uint32_t * p_audio_in_channels_32;     //audio buffer of one block with samples arranged as channels, for sample width of 24 or 32bit
+    uint16_t * p_audio_out_channels_16;     //audio buffer of one block of processed output samples, for sample width of 16bit
+    uint32_t * p_audio_out_channels_32;     //audio buffer of one block of processed output samples, for sample width of 24 or 32bit
     uint8_t * p_out_buf;
-    // buffer for Deinterleave the frame
-    // uint16_t** deinterleavedFrames;
-
-    // uint8_t in_buf[appconfDATA_FRAME_SIZE_BYTES] = {0};
-    // uint8_t out_buf[appconfDATA_FRAME_SIZE_BYTES] = {0};
+    size_t out_buf_size;
 
 
 
@@ -105,18 +152,6 @@ void audio_proc_test(void){
 
     printf("Input wav file bit depth : %d\n", input_header_struct.bit_depth);
     printf("Input wav file number of channels : %d\n", input_header_struct.num_channels);
-
-    // // Ensure 32bit wav file
-    // if(input_header_struct.bit_depth != 32)
-    // {
-    //     printf("Error: unsupported wav bit depth (%d) for %s file. Only 32 supported\n", input_header_struct.bit_depth, appconfINPUT_FILENAME);
-    //     _Exit(1);
-    // }
-    // // Ensure input wav file contains correct number of channels 
-    // if(input_header_struct.num_channels != appconfMAX_CHANNELS){
-    //     printf("Error: wav num channels(%d) does not match (%u)\n", input_header_struct.num_channels, appconfMAX_CHANNELS);
-    //     _Exit(1);
-    // }
     
     // Calculate number of frames in the wav file
     frame_count = wav_get_num_frames(&input_header_struct);
@@ -125,22 +160,24 @@ void audio_proc_test(void){
 
     p_in_buf = malloc(bytes_per_block);
 
+    out_buf_size = appconfFRAME_ADVANCE*appconfOUT_NUM_OF_CHANNELS*(input_header_struct.bit_depth/8);
+    p_out_buf = malloc(out_buf_size);
+
     switch (input_header_struct.bit_depth) {
         case 16:
-            p_audio_in_channels = (int16_t * )malloc(appconfFRAME_ADVANCE * sizeof(int16_t) * input_header_struct.num_channels);
+            p_audio_in_channels_16 = (uint16_t * )malloc(appconfFRAME_ADVANCE * sizeof(uint16_t) * input_header_struct.num_channels);
+            p_audio_out_channels_16 = (uint16_t * )malloc(appconfFRAME_ADVANCE * sizeof(uint16_t) * appconfOUT_NUM_OF_CHANNELS);
             break;
         case 24:
         case 32:
-            p_audio_in_channels = (int16_t * )malloc(appconfFRAME_ADVANCE * sizeof(int32_t) * input_header_struct.num_channels);
+            p_audio_in_channels_32 = (uint32_t * )malloc(appconfFRAME_ADVANCE * sizeof(uint32_t) * input_header_struct.num_channels);
+            p_audio_out_channels_32 = (uint32_t * )malloc(appconfFRAME_ADVANCE * sizeof(uint32_t) * appconfOUT_NUM_OF_CHANNELS);
             break;
         default:
             // Unsupported sample width
-            printf("Unsupported sample width: %d\n", input_header_struct.bit_depth);
+            printf("Unsupported sample width: %d!!\n", input_header_struct.bit_depth);
             break;
     }
-
-
-
 
     // Create output wav file
     wav_form_header(&output_header_struct,
@@ -157,30 +194,28 @@ void audio_proc_test(void){
 
     printf("Start running pipeline test, bytes_per_block%d\n",bytes_per_block);
 
-    for(unsigned b=0; b</*block_count*/1; b++) {
+    for(unsigned b=0; b<block_count; b++) {
         memset(p_in_buf, 0x00, bytes_per_block);
         long input_location =  wav_get_frame_start(&input_header_struct, b * appconfFRAME_ADVANCE, input_header_size);
         xscope_fseek(&infile, input_location, SEEK_SET);
         bytes_read = xscope_fread(&infile, p_in_buf, bytes_per_block);
-        memset(p_in_buf + bytes_read, 0x00, appconfDATA_FRAME_SIZE_BYTES - bytes_read);   //fill in buffer with zeros if data read is smaller than one frame data size.
+        memset(p_in_buf + bytes_read, 0x00, bytes_per_block - bytes_read);   //fill in buffer with zeros if data read is smaller than one frame data size.
 
-        deinterleaveFrame(p_in_buf, p_audio_in_channels, appconfFRAME_ADVANCE,input_header_struct.num_channels, input_header_struct.bit_depth );
-
-        printf("p_in_buf: 0x");
-        for(unsigned i=0;i<30;i++){
-            printf("%x ",p_in_buf[i]);
+        switch (input_header_struct.bit_depth) {
+            case 16:
+                deinterleaveFrame(p_in_buf, p_audio_in_channels_16, appconfFRAME_ADVANCE,input_header_struct.num_channels, input_header_struct.bit_depth );
+                process_frame(p_audio_in_channels_16, p_audio_out_channels_16, appconfOUT_NUM_OF_CHANNELS, input_header_struct.bit_depth);
+                interleaveFrame(p_out_buf, p_audio_out_channels_16, appconfFRAME_ADVANCE, appconfOUT_NUM_OF_CHANNELS, input_header_struct.bit_depth );
+                break;
+            case 24:
+            case 32:
+                deinterleaveFrame(p_in_buf, p_audio_in_channels_32, appconfFRAME_ADVANCE,input_header_struct.num_channels, input_header_struct.bit_depth );
+                process_frame(p_audio_in_channels_32, p_audio_out_channels_32, appconfOUT_NUM_OF_CHANNELS, input_header_struct.bit_depth);
+                interleaveFrame(p_out_buf, p_audio_out_channels_32, appconfFRAME_ADVANCE, appconfOUT_NUM_OF_CHANNELS, input_header_struct.bit_depth );
+                break;
         }
 
-        printf("\n p_audio_in_channels: 0x");
-        for(unsigned y=0;y<30;y++){
-            printf("%x ",p_audio_in_channels[y]);
-        }
-        printf("\n");
-
-
-        // process_frame(p_in_buf, p_out_buf);
-
-        // xscope_fwrite(&outfile, p_out_buf, appconfDATA_FRAME_SIZE_BYTES);
+        xscope_fwrite(&outfile, p_out_buf, out_buf_size);
     }
 
     printf("file processing finished!\n");
